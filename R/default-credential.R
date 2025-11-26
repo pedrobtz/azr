e1b541a3e80fa17b889d7721c05d650d27a99159
Pedro Baltazar <pedrobtz@gmail.com>
2025-11-26 23:13:28 +0100

    fix issues in graph client

--- R/default-credential.R
+++ R/default-credential.R
@@ -44,14 +44,15 @@
 #'
 #' @export
 get_token_provider <- function(
-    scope = NULL,
-    tenant_id = NULL,
-    client_id = NULL,
-    client_secret = NULL,
-    use_cache = "disk",
-    offline = TRUE,
-    chain = default_credential_chain(),
-    silent = TRUE) {
+  scope = NULL,
+  tenant_id = NULL,
+  client_id = NULL,
+  client_secret = NULL,
+  use_cache = "disk",
+  offline = TRUE,
+  chain = default_credential_chain(),
+  silent = TRUE
+) {
   crd <- get_credential_provider(
     scope = scope,
     tenant_id = tenant_id,
@@ -111,14 +112,15 @@ get_token_provider <- function(
 #'
 #' @export
 get_request_authorizer <- function(
-    scope = NULL,
-    tenant_id = NULL,
-    client_id = NULL,
-    client_secret = NULL,
-    use_cache = "disk",
-    offline = TRUE,
-    chain = default_credential_chain(),
-    silent = TRUE) {
+  scope = NULL,
+  tenant_id = NULL,
+  client_id = NULL,
+  client_secret = NULL,
+  use_cache = "disk",
+  offline = TRUE,
+  chain = default_credential_chain(),
+  silent = TRUE
+) {
   crd <- get_credential_provider(
     scope = scope,
     tenant_id = tenant_id,
@@ -177,14 +179,15 @@ get_request_authorizer <- function(
 #'
 #' @export
 get_token <- function(
-    scope = NULL,
-    tenant_id = NULL,
-    client_id = NULL,
-    client_secret = NULL,
-    use_cache = "disk",
-    offline = TRUE,
-    chain = default_credential_chain(),
-    silent = TRUE) {
+  scope = NULL,
+  tenant_id = NULL,
+  client_id = NULL,
+  client_secret = NULL,
+  use_cache = "disk",
+  offline = TRUE,
+  chain = default_credential_chain(),
+  silent = TRUE
+) {
   provider <- get_token_provider(
     scope = scope,
     tenant_id = tenant_id,
@@ -247,15 +250,16 @@ get_token <- function(
 #'
 #' @export
 get_credential_provider <- function(
-    scope = NULL,
-    tenant_id = NULL,
-    client_id = NULL,
-    client_secret = NULL,
-    use_cache = "disk",
-    offline = FALSE,
-    oauth_host = NULL,
-    oauth_endpoint = NULL,
-    chain = NULL) {
+  scope = NULL,
+  tenant_id = NULL,
+  client_id = NULL,
+  client_secret = NULL,
+  use_cache = "disk",
+  offline = FALSE,
+  oauth_host = NULL,
+  oauth_endpoint = NULL,
+  chain = NULL
+) {
   if (is.null(chain) || length(chain) == 0L) {
     chain <- default_credential_chain()
   }
@@ -320,14 +324,13 @@ get_credential_provider <- function(
 
   # All credentials failed, report all errors
   error_msgs <- c(
-    "All authentication methods in the chain failed!",
-    "x" = "Attempted credentials and their errors:"
+    "All authentication methods in the chain failed!"
   )
 
   for (cred_name in names(errors)) {
     error_msgs <- c(
       error_msgs,
-      " " = paste0("{.strong ", cred_name, "}:"),
+      "i" = paste0("{.strong ", cred_name, "}:"),
       "x" = errors[[cred_name]]
     )
   }
