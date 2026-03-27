a2469eb46703481973a36f6a62e433090cdef27e
pacbtz <paxbtz@gmail.com>
2026-03-27 13:22:19 +0100

    fix check

--- tests/testthat/_problems/test-graph-client-2.R
+++
@@ -1,9 +0,0 @@
-# Extracted from test-graph-client.R:2
-
-# setup ------------------------------------------------------------------------
-library(testthat)
-test_env <- simulate_test_env(package = "azr", path = "..")
-attach(test_env, warn.conflicts = FALSE)
-
-# test -------------------------------------------------------------------------
-graph <- azr_graph_client()
