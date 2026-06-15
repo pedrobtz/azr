7566b1604e1e1b2296c8ce8138915bedf09ad5e3
pedrobtz <pedrobtz@gmail.com>
2026-06-15 13:08:59 +0200

    add support for R4.2

--- R/azr-package.R
+++ R/azr-package.R
@@ -5,3 +5,7 @@
 #' @importFrom rlang %||% :=
 ## usethis namespace: end
 NULL
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
